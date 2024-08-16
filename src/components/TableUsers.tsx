4e3ae0d411178968fe9cd781bcf666c3fcc860ca
Franvz <franco.gonzalez12345123@gmail.com>
2024-08-16 00:18:40 -0300

    arreglar vista movil

--- src/components/TableUsers.tsx
+++ src/components/TableUsers.tsx
@@ -41,49 +41,51 @@ export function TableUsers () {
   }
 
   return (
-    <div className="container-table">
+    <>
       <button className="button-default-state" onClick={handleDefaultStateClick}>Estado Default</button>
-      <table className="table-users">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Email</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            users.map((user) => (
-              <tr key={user.id}>
-                <td>
-                  <p>{user.id}</p>
-                </td>
-                <td>
-                  <p>{user.name}</p>
-                </td>
-                <td>
-                  <p>{user.lastName}</p>
-                </td>
-                <td>
-                  <p>{user.email}</p>
-                </td>
-                <td>
-                  <div className="td-buttons">
-                    <button disabled={editingUser ? true : false} onClick={() => handleDeleteClick(user.id)}>
-                      <DeleteIcon />
-                    </button>
-                    <button onClick={() => handleEditClick(user.id)}>
-                      <EditIcon />
-                    </button>
-                  </div>
-                </td>
-              </tr>
-            ))
-          }
-        </tbody>
-      </table>
-    </div>
+      <div className="container-table">
+        <table className="table-users">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>Email</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users.map((user) => (
+                <tr key={user.id}>
+                  <td>
+                    <p>{user.id}</p>
+                  </td>
+                  <td>
+                    <p>{user.name}</p>
+                  </td>
+                  <td>
+                    <p>{user.lastName}</p>
+                  </td>
+                  <td>
+                    <p>{user.email}</p>
+                  </td>
+                  <td>
+                    <div className="td-buttons">
+                      <button disabled={editingUser ? true : false} onClick={() => handleDeleteClick(user.id)}>
+                        <DeleteIcon />
+                      </button>
+                      <button onClick={() => handleEditClick(user.id)}>
+                        <EditIcon />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }
